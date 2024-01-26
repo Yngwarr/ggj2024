b4f4c7ba362f53394498ba85029553271892493d
Yngvarr <k.yngvarr@gmail.com>
2024-01-26 18:50:59 +0100

    added an extra hand

--- src/Hand.gd
+++ src/Hand.gd
@@ -3,10 +3,18 @@ extends CharacterBody2D
 
 const INPUT_MULT := 5.0
 
+@export var right := false
+
 var input: Vector2
+var bounce_rotation: float = 50
 
 func _ready() -> void:
-	pass
+	if right:
+		$View.scale.x *= -1
+		$Collider.scale.x *= -1
+		$View.position.x *= -1
+		$Collider.position.x *= -1
+		bounce_rotation *= -1
 
 func _process(_delta: float) -> void:
 	input = Vector2(
@@ -19,9 +27,12 @@ func _physics_process(_delta: float) -> void:
 	if collision:
 		velocity = velocity.slide(collision.get_normal())
 
+	if name == "RightHand":
+		print("rotation ", rotation_degrees)
+
 	if Input.is_action_just_pressed("game_lbounce"):
 		var tween := create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-		tween.tween_property(self, "rotation_degrees", 50, .3) \
+		tween.tween_property(self, "rotation_degrees", bounce_rotation, .3) \
 			.set_trans(Tween.TRANS_CUBIC) \
 			.set_ease(Tween.EASE_OUT)
 		tween.chain() \
