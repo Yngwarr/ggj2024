d840477deafb445146b6198b17b0b4687284a7ad
Yngvarr <k.yngvarr@gmail.com>
2024-01-26 19:26:58 +0100

    mouse-controllable

--- src/Hand.gd
+++ src/Hand.gd
@@ -1,7 +1,7 @@
 class_name Hand
 extends CharacterBody2D
 
-const INPUT_MULT := 5.0
+const INPUT_MULT := 10.0
 
 @export var right := false
 
@@ -30,18 +30,12 @@ func _ready() -> void:
 		bounce_rotation *= -1
 
 func _process(_delta: float) -> void:
-	input = Vector2(
-		Input.get_axis(input_left, input_right),
-		Input.get_axis(input_up, input_down)
-	).normalized()
+	input = get_mouse_input() if right else get_keyboard_input()
 
 func _physics_process(_delta: float) -> void:
 	var collision := move_and_collide(input * INPUT_MULT)
 	if collision:
 		velocity = velocity.slide(collision.get_normal())
-
-	if name == "RightHand":
-		print("rotation ", rotation_degrees)
 
 	if Input.is_action_just_pressed(input_bounce):
 		var tween := create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
@@ -51,4 +45,18 @@ func _physics_process(_delta: float) -> void:
 		tween.chain() \
 			.tween_property(self, "rotation_degrees", 0, .3) \
 			.set_trans(Tween.TRANS_CUBIC) \
-			.set_ease(Tween.EASE_OUT) \
+			.set_ease(Tween.EASE_OUT)
+
+func get_keyboard_input() -> Vector2:
+	return Vector2(
+				Input.get_axis(input_left, input_right),
+				Input.get_axis(input_up, input_down)
+			).normalized()
+
+func get_mouse_input() -> Vector2:
+	var direction = get_global_mouse_position() - global_position
+
+	if direction.length() < 5 * INPUT_MULT:
+		return Vector2.ZERO
+
+	return direction.normalized()
