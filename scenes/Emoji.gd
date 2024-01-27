813bfa05c93af76a6c7e850a862a659fc1f6f2f4
Вероника Симагина <syudzius@gmail.com>
2024-01-27 11:59:34 +0100

    progress and arrow

---
+++ scenes/Emoji.gd
@@ -0,0 +1,12 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+#if progress >=6 then textureName="progress_happy"
+#if progress >=3 and progress <=5 then textureName="progress_smile"
+#if progress =1 or progress=2 then textureName="progress_neutral"
+#if progress=0 then textureName="progress_angry"
+
